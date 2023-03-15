b6e17e645a0929b1d6b5ba20af5668e3950b5a88
Carlos Alberto R <carlosalbertorios3@gmail.com>
2023-03-15 02:29:32 -0500

    remove files

--- src/app/models/post.model.ts
+++
@@ -1,5 +0,0 @@
-export interface IPost {
-  id: number;
-  name: string;
-  description: string;
-}
--- src/app/store/app.reducer.ts
+++
@@ -1,5 +0,0 @@
-import { IPost } from '../models/post.model';
-
-export interface AppState {
-  posts: { ids: Array<number>; entities: Array<IPost>; filterValue: string };
-}
